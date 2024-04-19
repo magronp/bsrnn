4cc8b732ad71d210547509059e2aeb3f07ee2aea
Paul Magron <paul.magron@inria.fr>
2024-04-19 19:28:49 +0200

    up

--- evaluate.py
+++ evaluate.py
@@ -36,7 +36,7 @@ def evaluate(args: DictConfig):
 
         # Special function to use parallel CPU
         if args.parallel_cpu:
-            args.eval_device="cpu" # make sure the device is CPU
+            args.eval.device="cpu" # make sure the device is CPU
             test_results = process_all_tracks_parallel(
                 args,
                 subset="test",
